f02b540d35bc01b1e19e767dfab901d5d6eb0e36
emerz <emerz@aventi.com.co>
2019-12-16 10:23:04 -0500

    Modify the RouteServiceProvider to add backend routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapBackendRoutes();
+
         //
     }
 
@@ -54,6 +56,14 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
+    }
+
+    protected function mapBackendRoutes()
+    {
+        Route::middleware('web')
+            ->prefix('admin')
+            ->namespace($this->namespace.'\Backend')
+            ->group(base_path('routes/backend.php'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Frontend
 Route::get('/', 'PageController@home')->name('home');
 
 Auth::routes();
